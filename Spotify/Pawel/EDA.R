edb268c5548d1e6ac65424d0a6ea5a4bbb3136cc
Ciechanowicz <Pawel.Ciechanowicz@staples-solutions.com>
2020-02-12 12:26:38 +0100

    eda

---
+++ Spotify/Pawel/EDA.R
@@ -0,0 +1,7 @@
+library(dplyr)
+
+mydata_train <- read.csv("C:/Data/ciechanp/My Documents/Repo1/DS-projekty/Spotify/train_set.csv", stringsAsFactors = FALSE)
+
+mydata_test <- read.csv("C:/Data/ciechanp/My Documents/Repo1/DS-projekty/Spotify/test_set.csv", stringsAsFactors = FALSE)
+
+
